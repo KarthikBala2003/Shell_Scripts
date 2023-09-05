7108c918a9fdb6f4240ae4d1f5cc6008588fcf86
Karthik Bala <KarthikBala2003@gmail.com>
2023-09-05 16:37:14 -0500

    Updated

--- Loops/xargs-example1.sh
+++ Loops/xargs-example1.sh
@@ -1,3 +1,4 @@
-
+#! /usr/bin/env bash
 
 grep "day" Iteration2.txt | cut -d',' -f2 | xargs -I TIME echo 'TIME'
+# grep "day" Iteration2.txt | cut -d',' -f1-2 | cut -d' ' -f4-6 | xargs -n 3 bash -c 'echo $2'
