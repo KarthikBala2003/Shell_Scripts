2b692d63dfb32a73d80256518e4ca7bcb679eacf
Karthik Bala <KarthikBala2003@gmail.com>
2023-09-07 08:00:12 -0500

    Array length validated for MemFree

--- Loops/old-device-metrics.sh
+++ Loops/old-device-metrics.sh
@@ -17,20 +17,18 @@ print_metrics_arrays() {
   uptime_array=()
   readarray -t uptime_array < <(grep "day" Iteration2.txt | cut -d',' -f1-2 | cut -d' ' -f4-6 | xargs -I UP echo 'UP') 
 
-  # memfree_array=()
-  # readarray -t memfree_array < <(grep "MemFree:" Iteration2.txt | cut -d':' -f2 | xargs -I MF echo 'MF')
-   
+  memfree_array=()
+  readarray -t memfree_array < <(grep "MemFree:" Iteration2.txt | cut -d':' -f2 | xargs -I MF echo 'MF')
+
+  len_memfree_array=${#memfree_array[@]}
+  memfree_avg="$((len_memfree_array/3))"
+     
   # Check if all three arrays have the same length
-  # if [ ${#mnemonic_array[@]} -ne ${#uptime_array[@]} ] || [ ${#mnemonic_array[@]} -ne ${#memfree_array[@]} ]; then
-  #     echo "Error: All arrays must have the same length."
-  #     return 1
-  # fi
-
-  if [ ${#mnemonic_array[@]} -ne ${#uptime_array[@]} ]; then
-    echo "Error: All arrays must have the same length."
-    return 1
+  if [ ${#mnemonic_array[@]} -ne ${#uptime_array[@]} ] || [ ${#mnemonic_array[@]} -ne $memfree_avg ]; then
+      echo -e "Error: All arrays must have the same length.\n$header1: ${#mnemonic_array[@]}  $header2: ${#uptime_array[@]}  $header3: $len_memfree_array"
+      return 1
   fi
-
+  
   for i in "${!mnemonic_array[@]}"; do
   
     mnemonic="${mnemonic_array[i]//[$'\r']}"
@@ -38,7 +36,9 @@ print_metrics_arrays() {
     printf "%-15s %-15s %s\n" "$mnemonic" "$uptime" >> metrics.txt
     
   done
+
 }
+
 
 # Call the function to print the arrays
 print_metrics_arrays
